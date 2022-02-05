6838564d187afad457fb7cc9e0eb1c3b87fc9968
Andrew Sveshnikov <andrey.sveshnikov@softlabs.com>
2022-02-05 17:42:47 +0700

    clean

--- app/src/main/java/com/example/tinkoffapp/presentation/DevelopersLifeActivity.kt
+++ app/src/main/java/com/example/tinkoffapp/presentation/DevelopersLifeActivity.kt
@@ -1,9 +1,10 @@
-package com.example.tinkoffapp
+package com.example.tinkoffapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tinkoffapp.R
 
-class MainActivity : AppCompatActivity() {
+class DevelopersLifeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
